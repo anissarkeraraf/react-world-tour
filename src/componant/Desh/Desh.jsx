86cdee1937cf62b37f8f1a808090910ba122b629
anissarkeraraf <anissarkeraraf@gmail.com>
2024-03-11 17:36:38 +0600

    country info

--- src/componant/Desh/Desh.jsx
+++ src/componant/Desh/Desh.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import'./desh.css'
 const Desh = ({desh}) => {
    const {name, flags, population, area, cca3} = desh;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () =>{
+        setVisited(!visited)
+    }
+
     return (
-        <div className='box'>
-            <h3>Name: {name.common}</h3>
+        <div className={`box ${visited && 'visited'}`}>
+            <h3 style={{color: visited? 'purple' : 'white'}}>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
-            <p>Code: {cca3}</p>
+            <p><small>Code: {cca3}</small></p>
+            <button onClick={handleVisited}>{visited? 'Visited' : 'Going'}</button>
+            {visited? 'I have visited this country' : 'I want to visit'}
         </div>
     );
 };
