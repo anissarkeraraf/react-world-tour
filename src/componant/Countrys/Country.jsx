15a4f1b8fd4f3e0daac0bfd7896d035ac6fcfdca
anissarkeraraf <anissarkeraraf@gmail.com>
2024-03-12 15:35:03 +0600

    visited info

--- src/componant/Countrys/Country.jsx
+++ src/componant/Countrys/Country.jsx
@@ -2,9 +2,12 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Desh from "../Desh/Desh";
 import './Country.css'
+import { Linter } from "eslint";
 
 const Country = () => {
     const [countries, setCountries] = useState([]);
+
+    const [visitedCountry, setVisitedCountry] = useState([]);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
@@ -12,12 +15,35 @@ const Country = () => {
             .then(data => setCountries(data))
     }, [])
 
+    const handleVisitedCountry = country => {
+        console.log('add this to your visited country');
+        // console.log(country);
+        // const newVisitedCountry = [...visitedCountry, country]
+        // setVisitedCountry(newVisitedCountry)
+    }
+
+
+
     return (
         <div>
+
             <h3>Countries: {countries.length}</h3>
+
+            <div>
+                <h4>Visited Country: {visitedCountry.length}</h4>
+                <ul>
+                    {
+                        visitedCountry.map(country => <li>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
+
             <div className="country-container">
                 {
-                    countries.map(anis => <Desh key={anis.cca3} desh={anis}></Desh>)
+                    countries.map(anis => <Desh
+                        key={anis.cca3}
+                        handleVisitedCountry={handleVisitedCountry}
+                        desh={anis}></Desh>)
                 }
             </div>
         </div>
--- src/componant/Desh/Desh.jsx
+++ src/componant/Desh/Desh.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import'./desh.css'
-const Desh = ({desh}) => {
+const Desh = ({desh, handleVisitedCountry}) => {
    const {name, flags, population, area, cca3} = desh;
 
     const [visited, setVisited] = useState(false);
@@ -9,6 +9,12 @@ const Desh = ({desh}) => {
         setVisited(!visited)
     }
 
+    const passWithParam = () =>{
+        handleVisitedCountry(desh)
+    }
+
+    console.log(handleVisitedCountry)
+
     return (
         <div className={`box ${visited && 'visited'}`}>
             <h3 style={{color: visited? 'purple' : 'white'}}>Name: {name.common}</h3>
@@ -16,6 +22,7 @@ const Desh = ({desh}) => {
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p><small>Code: {cca3}</small></p>
+            <button onClick={passWithParam}>Mark Visited</button> <br/><br/>
             <button onClick={handleVisited}>{visited? 'Visited' : 'Going'}</button>
             {visited? 'I have visited this country' : 'I want to visit'}
         </div>
